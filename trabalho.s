7dbfe5b07fbb65a46f406a45ffaaa90503216643
poledna0 <henriquepoledna@proton.me>
2025-09-14 13:47:25 -0300

    fim trabalho.s

--- trabalho.s
+++ trabalho.s
@@ -12,7 +12,7 @@ soma:
     movq 24(%rbp), %rbx       # rbx = y (endereço)
     movq (%rbx), %rdx         # rdx = *y
 
-    cmpq %rdx, %rcx           # compara *x >= *y?
+    cmpq %rdx, %rcx           # *x >= *y
     jle casonao               # se *x < *y, vai pro else
 
     addq %rdx, %rcx           # tmp = *x + *y
@@ -41,20 +41,20 @@ _start:
     # B
     movq $3, -16(%rbp)
 
-    movq %rbp, %rax           # rax = %rbp
+    movq %rbp, %rax
     subq $8, %rax             # rax = endereço de A
 
     movq %rbp, %rbx           # rbx = %rbp
     subq $16, %rbx            # rbx = endereço de B
 
-    pushq %rbx                # arg2: endereço de B
-    pushq %rax                # arg1: endereço de A
+    pushq %rbx
+    pushq %rax
 
     call soma
 
-    addq $16, %rsp            # limpa os dois pushes (2x8 bytes)
+    addq $16, %rsp            # limpa os dois pushes
 
-    movq -8(%rbp), %rdi       # rdi = A (valor resultante)
+    movq -8(%rbp), %rdi
 
-    movq $60, %rax            # syscall: exit
+    movq $60, %rax
     syscall
